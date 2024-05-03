a21fa09e398ed5db36b9740c17932d5d1e53d518
cateschmidt <c8schmidt@gmail.com>
2024-05-02 22:31:33 -0400

    initial commit

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+// A new class is defined
+class HelloWorld {
+    // call to main
+    public static void main(String[] args) {
+        // print Hello World!
+        System.out.println("Hello World!"); 
+        // Hello World!
+    }
+}
